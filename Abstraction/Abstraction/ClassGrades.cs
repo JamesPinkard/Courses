f0e3c06b51e35e1258770c415b5b276115a60919
JamesPinkard <James.b.Pinkard@gmail.com>
2014-11-23 10:57:50 -0600

    Started HW1 Problem 5

---
+++ Abstraction/Abstraction/ClassGrades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class ClassGrades
+    {
+        private List<int> grades;
+
+        public int[] Grades
+        {
+            get { return grades.ToArray(); }            
+        }
+
+        public void LoadGrades(string file)
+        {
+            
+        }
+    }
+}
