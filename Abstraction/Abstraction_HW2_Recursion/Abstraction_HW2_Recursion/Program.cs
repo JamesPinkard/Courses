4e5518ddd8bf0970d77f805dd55e41a7bbd45a05
JamesPinkard <James.b.Pinkard@gmail.com>
2014-12-09 08:43:10 -0600

    Started on Homework 3 Recursion

---
+++ Abstraction/Abstraction_HW2_Recursion/Abstraction_HW2_Recursion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_HW2_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Enter Number:");
+            string mynum = Console.ReadLine();
+            int num = int.Parse(mynum);
+            PrintInBinary(num);
+            Console.ReadKey();
+        }
+
+        static void PrintInBinary(int num)
+        {
+            if (num==0)
+            {
+                Console.Write(0);
+            }
+            else
+            {
+                int bit = num % 2;
+                int half = num / 2;
+                PrintInBinary(half);
+                Console.Write(bit);
+            }
+        }
+
+        /*
+        static bool CanMakeSum(List<int> nums, int targetSum)
+        {
+            if (nums.Count == 0)
+                return false;
+            else
+            {
+                int first = nums.First();
+                var remaining = nums.GetRange(1, nums.Count - 1);
+                CanMakeSum(remaining, targetSum);               
+            }
+        }
+         */
+    }
+}
---
+++ Abstraction/Abstraction_HW2_Recursion/Lecture_Recursion/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int Raise(int baseNum, int exp)
+        {
+            if (exp == 0)
+                return 1;
+            else
+                return baseNum * Raise(baseNum, exp - 1);
+        }
+
+        static int EfficientRaise(int baseNum, int exp)
+        {
+            if (exp == 0)
+                return 1;
+            else
+            {
+                int half = Raise(baseNum, exp / 2);
+                if (exp % 2 == 0)
+                    return half * half;
+                else
+                    return baseNum * half * half;
+            }
+        }
+
+        static bool IsPalindrome(string s)
+        {
+            if (s.Length <= 1) return true;
+            return s[0] == s[s.Length - 1] && IsPalindrome(s.Substring(1, s.Length - 2));
+        }
+
+        static readonly int NOTFOUND = -1;
+        static int BSearch(List<int> numbers, int start, int stop, int key)
+        {
+            if (start > stop) return NOTFOUND;
+
+            int mid = (start + stop) / 2;
+            if (key == numbers[mid])
+                return mid;
+            else if (key < numbers[mid])
+                return BSearch(numbers, start, mid - 1, key);
+            else
+                return BSearch(numbers, mid + 1, stop, key);
+        }
+
+        static int C(int n, int k)
+        {
+            if (k == 0 || k == n)
+                return 1;
+            else
+                return C(n - 1, k) + C(n - 1, k - 1);
+        }
+
+        static void RecPermute(string soFar, string rest)
+        {
+            if (rest == "")
+                Console.WriteLine(soFar);
+            else
+            {
+                for (int i = 0; i < rest.Length; i++)
+                {
+                    string next = soFar + rest[i];
+                    string remaining = rest.Substring(0, i) + rest.Substring(i + 1);
+                    RecPermute(next, remaining);
+                }
+            }
+        }
+
+        // wrapper function
+        void ListPermutations(string s)
+        {
+            RecPermute("", s);
+        }
+    }
+}
